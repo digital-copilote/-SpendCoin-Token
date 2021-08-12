9b024b0a38f8a39e2f19f5fa6f0f26db3d05b8d6
Mickaël Bouvier <mickael.bouvier.pro@gmail.com>
2021-08-12 16:19:15 +0200

    contrat SPC v1 + tests unitaires

--- contracts/SPC_Token.sol
+++ contracts/SPC_Token.sol
@@ -1,9 +1,20 @@
-//SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: GNU AGPLv3
 pragma solidity ^0.8.4;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+//import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
 
-contract SPC_Token {
-  
+/// @title contract for SpendCoin
+/// @author Olivier Fernandez / Mickaël Bouvier / Anis Boussedra
+/// @notice create token : SpendCoin
+
+contract SPC_Token is ERC20 {
+
+    constructor() ERC20("SpendCoin", "SPC") {
+        //ownerContract = msg.sender;
+		_mint(msg.sender, 80000000 * 10**18);
+	}
+
+
 }
--- test/SPC_Token-test.js
+++ test/SPC_Token-test.js
@@ -1,18 +1,43 @@
 const { expect } = require("chai");
 
-xdescribe("Greeter", function () {
-  xit("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+const totalSupply = ethers.utils.parseEther(new String(80 * 10**6).valueOf());
+// const totalSupply = "80000000000000000000000000";
+let SPC_Token;
+let token;
+let owner, addr1, addr2, addr3, addrs
+let owneraddr;
+let contractAsSigner0, contractAsSigner1, contractAsSigner2;
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+beforeEach(async function () {
+  // Get the ContractFactory and Signers here.
+  SPC_Token = await ethers.getContractFactory("SPC_Token");
+  token = await SPC_Token.deploy();
+  await token.deployed();
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+  [owner, addr1, addr2, addr3, ...addrs] = await ethers.getSigners();
+  owneraddr = owner.address;
+  console.log("owner address: " + owneraddr);
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+  contractAsSigner0 = token.connect(owner);
+  contractAsSigner1 = token.connect(addr1);
+  contractAsSigner2 = token.connect(addr2);
+});
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+
+describe("SPC_Token", function () {
+  it("Should return the name", async function () {
+    expect(await token.name()).to.equal("SpendCoin");
+  });
+  
+  it("Should return the symbol", async function () {
+    expect(await token.symbol()).to.equal("SPC");
+  });
+
+  it("Should return the total supply", async function () {
+    expect(await token.totalSupply()).to.equal(totalSupply);
+    // expect(await token.totalSupply()).to.equal(totalSupply);
+    // expect((await token.totalSupply()).toString()).to.equal(totalSupply);
+    
+    expect(await token.balanceOf(owneraddr)).to.equal(totalSupply);
   });
 });
