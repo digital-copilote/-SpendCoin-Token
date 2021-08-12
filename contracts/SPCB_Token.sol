0abe155cc7de431be97287513bc6258f0ce2fdb5
Mickaël Bouvier <mickael.bouvier.pro@gmail.com>
2021-08-12 12:29:35 +0200

    ajout dépendance openzeppelin + modif version solidity

--- contracts/SPCB_Token.sol
+++ contracts/SPCB_Token.sol
@@ -1,8 +1,8 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.6;
+pragma solidity ^0.8.4;
 
 import "hardhat/console.sol";
-
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SPCB_Token {
   
